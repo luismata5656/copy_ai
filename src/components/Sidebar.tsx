317bac7b423345ccc541efedb1fed43971ad747b
luismata5656 <luismata@luishmata.com>
2023-11-10 13:46:27 -0700

    top sidebar controls

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -5,18 +5,27 @@ import ChatItem from '@/components/chat/Chat'
 export default function Sidebar() {
 return (
 	<div className='flex flex-col h-full text-default-white w-1/5 h-full bg-default-secondary'>
-	<div className='h-16'>
-		Top Controls
+	<div className='mx-4 mt-4 h-16'>
+		<div className='w-full flex gap-4'>
+		<button className='w-5/6 btn rounded-md text-left border border-solid border-1 border-default-secondary-3 p-1 hover:bg-default-secondary-3'>
+			New Chat
+		</button>
+		<button className='w-1/6 border border-default-secondary-3 rounded-md p-1'>
+			S
+		</button>
+		</div>
 	</div>
-	<div className='flex-1 w-full flex flex-col items-center overflow-y-auto'> 
-		<ChatItem name="Chat 1"/>
+	<div className='mx-4 flex-1 overflow-y-auto'> 
+		<div className='w-full flex flex-col gap-4'>
+		<ChatItem name="Chat 1123"/>
 		<ChatItem name="Chat 2"/>
 		<ChatItem name="Chat 3"/>
 		<ChatItem name="Chat 4"/>
 		<ChatItem name="Chat 5"/>
+		</div>
 	</div>
 	<div>
-		Bottom Controls
+		
 	</div>
 	</div>
 )
--- src/components/chat/Chat.tsx
+++ src/components/chat/Chat.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 export default function ChatItem({name}:{name:string}) {
 return (
-	<div className='border border-solid border-1 border-default-secondary-2 m-2 p-1 w-10/12'> 
+	<button className='text-left border border-solid border-1 border-default-secondary-2 p-1 rounded-md w-full hover:bg-default-secondary-3'> 
 		{name}
-	</div>
+	</button>
 
 )
 }
